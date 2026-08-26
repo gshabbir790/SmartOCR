bd55b6da551aa9fb71cca6362ca9625cad7dd85d
gshabbir790 <ik7952683@gmail.com>
2026-08-26 17:51:48 +0500

    Add files via upload

--- lib/features/ocr/ocr_result_screen.dart
+++ lib/features/ocr/ocr_result_screen.dart
@@ -68,18 +68,10 @@ class _OcrResultScreenState extends State<OcrResultScreen> {
     await launchUrl(uri, mode: LaunchMode.externalApplication);
   }
 
-  bool _isRtlText(String text) {
-    final rtl = RegExp(r'[\u0590-\u08FF]');
-    final ltr = RegExp(r'[A-Za-z]');
-    final rtlCount = rtl.allMatches(text).length;
-    final ltrCount = ltr.allMatches(text).length;
-    return rtlCount > 0 && rtlCount >= ltrCount;
-  }
-
   @override
   Widget build(BuildContext context) {
     final imageExists = File(widget.item.path).existsSync();
-    final isRtl = _isRtlText(_text.text);
+    final isRtl = RegExp(r'[\u0600-\u06FF\u0750-\u077F]').hasMatch(_text.text);
 
     return Scaffold(
       appBar: AppBar(
@@ -121,14 +113,8 @@ class _OcrResultScreenState extends State<OcrResultScreen> {
             maxLines: null,
             textDirection: isRtl ? TextDirection.rtl : TextDirection.ltr,
             textAlign: isRtl ? TextAlign.right : TextAlign.left,
-            style: const TextStyle(
-              fontSize: 17,
-              height: 1.8,
-              letterSpacing: 0,
-            ),
             decoration: const InputDecoration(
               hintText: 'Extracted text',
-              alignLabelWithHint: true,
             ),
           ),
           const SizedBox(height: 14),
